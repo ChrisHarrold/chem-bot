bf138c266784f9984ed7caf846a20ca49b6631eb
Chris Harrold <christophereharrold@hotmail.com>
2018-01-31 13:48:44 -0800

    camera additives

--- chem-bot/pi_prime.py
+++ chem-bot/pi_prime.py
@@ -1,8 +1,8 @@
 import json
-#import picamera
+import picamera
 from watson_developer_cloud import VisualRecognitionV3
 
-#camera = picamera.PiCamera()
+camera = picamera.PiCamera()
 visual_recognition = VisualRecognitionV3(
     '2016-05-20',
     api_key='6c9bd2eebc2ca826e9ef864eb8934fdd4c5d259a')
@@ -25,8 +25,8 @@ def recognize():
         print("Not a high enough score")
 
 def capture():
-    print("I am taking a picture! (Not really)")
-    #camera.capture('/tmp/image.jpg')
+    print("I am taking a picture! (Yes, really!)")
+    camera.capture('/tmp/image.jpg')
 
 def main():
     capture()
